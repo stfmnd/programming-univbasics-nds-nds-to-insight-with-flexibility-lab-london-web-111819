f66421108cfab994ea84e9fb90021c1163e57604
Matthew <matthew.97.clark@gmail.com>
2019-10-31 00:38:31 +0000

    Done.

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -22,33 +22,31 @@ end
 
 def movie_with_director_name(director_name, movie_data)
   { 
+    :director_name => director_name,
     :title => movie_data[:title],
     :worldwide_gross => movie_data[:worldwide_gross],
     :release_year => movie_data[:release_year],
-    :studio => movie_data[:studio],
-    :director_name => director_name
+    :studio => movie_data[:studio]
   }
+
 end
 
+name = 'Matthew'
+example = directors_database[0][:movies][0]
+pp movie_with_director_name(name, example)
 
 # Your code after this point
 
 def movies_with_director_key(name, movies_collection)
-  # GOAL: For each Hash in an Array (movies_collection), provide a collection
-  # of movies and a directors name to the movie_with_director_name method
-  # and accumulate the returned Array of movies into a new Array that's
-  # returned by this method.
-  #
-  # INPUT:
-  # * name: A director's name
-  # * movies_collection: An Array of Hashes where each Hash represents a movie
-  #
-  # RETURN:
-  #
-  # Array of Hashes where each Hash represents a movie; however, they should all have a
-  # :director_name key. This addition can be done by using the provided
-  # movie_with_director_name method
+ counter = 0
+ new_array = []
+ while counter < movies_collection.length do 
+   new_array.push(movie_with_director_name(name, movies_collection[counter]))
+   counter += 1
+ end
+ new_array
 end
+
 
 
 def gross_per_studio(collection)
