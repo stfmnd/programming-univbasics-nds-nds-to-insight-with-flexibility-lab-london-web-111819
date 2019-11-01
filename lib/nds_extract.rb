2a2d9805ee4346902ddae926c236449e02255df5
Matthew <matthew.97.clark@gmail.com>
2019-11-01 22:08:30 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -57,8 +57,9 @@ def gross_per_studio(collection)
   
   # Hash whose keys are the studio names and whose values are the sum
   # total of all the worldwide_gross numbers for every movie in the input Hash
-  binding.pry
-  pp collection
+  # binding.pry
+  counter = 0
+  while c
 end
 
 def movies_with_directors_set(source)
